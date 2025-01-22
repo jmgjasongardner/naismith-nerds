02ab16af8c45c05c2e5e1bd95c434840a2a009f4
Jason Gardner <jmgjasongardner@gmail.com>
2025-01-22 00:06:50 -0500

    create initial rapm funs and boiler code

---
+++ util_funs.py
@@ -0,0 +1,63 @@
+import polars as pl
+import pandas as pd
+
+def pull_in_data() -> tuple[pl.DataFrame, pl.DataFrame]:
+    data = "CollectiveBball/GameResults.xlsm"
+    df = pl.from_pandas(pd.read_excel(data, sheet_name="GameResults", engine="openpyxl"))
+    tiers = pl.from_pandas(pd.read_excel(data, sheet_name="PlayerTiers", engine="openpyxl"))
+
+    return df, tiers
+
+def clean_data(df: pl.DataFrame) -> pl.DataFrame:
+    return (df.with_columns(
+            pl.when(pl.col("B_SCORE") > pl.col("A_SCORE"))
+            .then(pl.lit("B"))
+            .when(pl.col("B_SCORE") < pl.col("A_SCORE"))
+            .then(pl.lit("A"))
+            .otherwise(pl.lit("Error"))
+            .alias("Winner")
+        )
+        .with_columns(
+            pl.col("Date")
+            .dt.strftime("%Y-%m-%d")
+            .alias("GameDate")  # Format the date to YYYY-MM-DD
+        )
+        .with_columns(
+            pl.col("Date")
+            .cum_count()
+            .over("GameDate")
+            .cast(pl.Int32)
+            .alias("GameNum")  # Sequential count per Date
+        )
+        .with_columns(
+            (pl.col("GameDate") + "-" + (pl.col("GameNum")).cast(pl.Utf8)).alias("GameId")
+        )
+        .drop("GameDate")
+        .filter(pl.col("A_SCORE").is_not_nan()))
+
+def player_data(df: pl.DataFrame) -> pl.DataFrame:
+    # Reshape the dataframe with unpivot
+    team_cols = [f"A{i}" for i in range(1, 6)] + [f"B{i}" for i in range(1, 6)]
+    id_cols = ["GameId", "A_SCORE", "B_SCORE", "Winner"]
+
+    return (
+        df.select(id_cols + team_cols)
+        .unpivot(index=id_cols)
+        .with_columns(
+            pl.col("variable").str.extract(r"([AB])", 1).alias("team"),
+            pl.col("value").alias("player"),
+        )
+        .drop(["value", "variable"])
+        .with_columns(
+            pl.when(pl.col("team") == "A")
+            .then(pl.col("A_SCORE"))
+            .otherwise(pl.col("B_SCORE"))
+            .alias("team_score"),
+            pl.when(pl.col("team") == "A")
+            .then(pl.col("B_SCORE"))
+            .otherwise(pl.col("A_SCORE"))
+            .alias("opponent_score"),
+            (pl.col("team") == pl.col("Winner")).cast(pl.Int8).alias("GameWon"),
+        )
+        .with_columns((pl.col("team_score") - pl.col("opponent_score")).alias("point_diff"))
+    )
