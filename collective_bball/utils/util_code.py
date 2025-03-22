df435c7807f3dea2edd675fc9abe6d23d0b6f3c4
Jason Gardner <jmgjasongardner@gmail.com>
2025-03-22 14:25:59 -0400

    fix url

--- collective_bball/utils/util_code.py
+++ collective_bball/utils/util_code.py
@@ -1,2 +1,2 @@
-public_data_url = "https://onedrive.live.com/download?resid=FED143F0BF36343C%21313448&authkey=!AJsr-ux_p3t8PaE"
+public_data_url = "https://onedrive.live.com/download?resid=FED143F0BF36343C%21369695&authkey=!ANOKKKwb2CMPMB8"
 player_columns = [f"A{i}" for i in range(1, 6)] + [f"B{i}" for i in range(1, 6)]
