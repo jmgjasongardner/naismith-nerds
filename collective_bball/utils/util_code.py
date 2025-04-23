e046882a92f65b139574e363b217de619c259ab5
Jason Gardner <jmgjasongardner@gmail.com>
2025-04-22 21:15:31 -0400

    pull from local instead of onedrive

--- collective_bball/utils/util_code.py
+++ collective_bball/utils/util_code.py
@@ -1,2 +1,3 @@
-public_data_url = "https://onedrive.live.com/download?resid=FED143F0BF36343C%21369695&authkey=!ANOKKKwb2CMPMB8"
+#public_data_url = "https://onedrive.live.com/download?resid=FED143F0BF36343C%21369695&authkey=!ANOKKKwb2CMPMB8"
+public_data_url = "collective_bball/GameResults.xlsm"
 player_columns = [f"A{i}" for i in range(1, 6)] + [f"B{i}" for i in range(1, 6)]
