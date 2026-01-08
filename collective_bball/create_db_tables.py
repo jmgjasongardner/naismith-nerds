2d092777120431282b6ad1712f1723583ba99f7e
Jason Gardner <jmgjasongardner@gmail.com>
2026-01-07 21:26:37 -0500

    Jan 7 results

--- collective_bball/create_db_tables.py
+++ collective_bball/create_db_tables.py
@@ -18,3 +18,21 @@ if __name__ == "__main__":
     conn = duckdb.connect("bball_database.duckdb")
     create_tables(conn)
     conn.close()
+
+
+# conn = duckdb.connect("bball_database.duckdb")
+#
+# # Delete unwanted players
+# conn.execute("""
+#     DELETE FROM ratings
+#     WHERE player IN ('Name1', 'Name2');
+# """)
+#
+# # Update a player name
+# conn.execute("""
+#     UPDATE ratings
+#     SET player = 'WillS'
+#     WHERE player = 'WWillStache';
+# """)
+#
+# conn.close()
